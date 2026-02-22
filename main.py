d51936549aa2bf1b96311b98bafc9da0d8b1c180
irakli <samsaxuri123@gmail.com>
2026-02-22 20:36:18 +0400

    removed debug blocking port listening

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
-
 from src import create_app
 from src.database.extensions import db
-
 from src.core.user import User
 
 app = create_app()
@@ -11,8 +9,10 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Database tables created successfully!")
-
-    app.run(debug=True)
-    if os.getenv("FLASK_ENV") == "PROD":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+    is_prod = os.getenv("FLASK_ENV") == "PROD"
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host='0.0.0.0' if is_prod else '127.0.0.1',
+        port=port,
+        debug=not is_prod
+    )
