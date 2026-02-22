3b1fc61c34e2fadabfd86f551c867db70f92313c
irakli <samsaxuri123@gmail.com>
2026-02-22 20:41:35 +0400

    retarded gemini

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Database tables created successfully!")
-    is_prod = os.getenv("FLASK_ENV") == "PROD"
+    env = os.getenv("FLASK_ENV")
     port = int(os.environ.get("PORT", 5000))
     app.run(
-        host='0.0.0.0' if is_prod else '127.0.0.1',
+        host='0.0.0.0' if env == "PROD" else '127.0.0.1',
         port=port,
-        debug=not is_prod
+        debug=False if env == "PROD" else True,
     )
